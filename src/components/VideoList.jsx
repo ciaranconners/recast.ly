fa25225967ae1c4cce6bc73787a9d890fcbbf7a3
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-06-03 13:44:27 -0400

    On Tests for VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,26 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    {props.videos.map((video) =>
+      <VideoListEntry entry={video}/>)
+    }
   </div>
 );
 
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map((todo, index) =>
+//       <TodoListItem todo={todo} key={index} />
+//     )}
+//   </ul>
+// );
+
+
+
+// App is entrypoint for data, constructor only for class components
+
+
 // PropTypes tell other developers what `props` a component expects
+
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
@@ -21,3 +29,12 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+
+
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,7 +1,7 @@
-var VideoListEntry = () => (
+var VideoListEntry = (videos) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      <img className="media-object" src={videos.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">Video Title</div>
@@ -9,6 +9,16 @@ var VideoListEntry = () => (
     </div>
   </div>
 );
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       done: false
+//     };
+//   }
+// }
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
